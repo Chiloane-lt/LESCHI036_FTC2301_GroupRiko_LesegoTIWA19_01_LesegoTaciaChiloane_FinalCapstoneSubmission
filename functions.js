c9fc60d39c55c9e26888007cdedb3d1c6e2de739
Chiloane-lt <chiloane.lt@hotmail.com>
2023-05-05 12:46:21 +0200

    Update JSDoc for all code.

--- functions.js
+++ functions.js
@@ -1,89 +1,11 @@
 import { authors, books, BOOKS_PER_PAGE } from './data.js'
 
-/**
- * Creates an html fragment given an object.
- * 
- * @param {array} props is an object array with book properties.
- * @returns {HTMLElement} 
- **/
-export const createPreview = (props) => {
-    const { author, image, title, id } = props;
-
-    const newElement = document.createElement('button');
-    newElement.className = 'preview';
-    newElement.setAttribute('data-preview', id);
-
-    newElement.innerHTML =  /* HTML */`
-        <img
-                class="preview__image"
-                src="${image}"
-            />
-            
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${author}</div>
-            </div>
-        `
-
-    return newElement;
-};
-
-export const createGenres = (obj) => {
-    let genresHtml = document.createDocumentFragment();
-    let element = document.createElement('option');
-
-};
-
-
-/**
- * Creates a slice of specified length from the database of books.
- * 
- * @param {array} array is an object array of books with properties.
- * @param {number} start is a number denoting where to start slice.
- * @param {number} end is a number denoting where to end slice.
- * @return {HTMLElement} 
- * 
- */
-export const createPreviewsFragment  = (array , start, end) => {
-    
-    const booksSlice = array.slice(start, end);
-
-    let previewFragment = document.createDocumentFragment();
-
-    for (let i = 0; i < booksSlice.length; i++){
-
-        let { author, image, title, id } = booksSlice[i];
-        author = authors[author];
-    
-        const preview = {
-            author,
-            id,
-            image,
-            title,
-        };
-        previewFragment.appendChild(createPreview(preview));
-    };
-    return previewFragment;
-};
-
-
-/**
- * Determines the number of pages to travese on the app based on the total
- * number of books available in the database.
- * 
- * @param {array} array with total number of books.
- * @param {number} page current page.
- * @returns {number}
- */
-export const updateRemaining = (array, page) => {
-    let remaining = array.length - (page * BOOKS_PER_PAGE);
-    return remaining;
-}; 
+// DOM Data
 
 export const html = {
     view: {
         mainHtml: document.querySelector('[data-list-items]'),
-    },    
+    },
     scroll: {
         moreButton: document.querySelector('[data-list-button]'),
     },
@@ -117,39 +39,201 @@ export const html = {
     },
 };
 
+/**
+ * Creates an html fragment given an object.
+ * 
+ * @param {array} props is an object array with book properties.
+ * @returns {HTMLElement} 
+ **/
+export const createPreview = (props) => {
+    const { author, image, title, id } = props;
+
+    const newElement = document.createElement('button');
+    newElement.className = 'preview';
+    newElement.setAttribute('data-preview', id);
+
+    newElement.innerHTML =  /* HTML */`
+        <img
+                class="preview__image"
+                src="${image}"
+            />
+            
+            <div class="preview__info">
+                <h3 class="preview__title">${title}</h3>
+                <div class="preview__author">${author}</div>
+            </div>
+        `
+
+    return newElement;
+};
 
 /**
- * Event to show summary overlay after clicking preview button.
+ * Creates a slice of specified length from the database of books.
+ * 
+ * @param {array} array is an object array of books with properties.
+ * @param {number} start is a number denoting where to start slice.
+ * @param {number} end is a number denoting where to end slice.
+ * @return {HTMLElement} 
+ * 
+ */
+export const createPreviewsFragment = (array, start, end) => {
+
+    const booksSlice = array.slice(start, end);
+
+    let previewFragment = document.createDocumentFragment();
+
+    for (let i = 0; i < booksSlice.length; i++) {
+
+        let { author, image, title, id } = booksSlice[i];
+        author = authors[author];
+
+        const preview = {
+            author,
+            id,
+            image,
+            title,
+        };
+        previewFragment.appendChild(createPreview(preview));
+    };
+    return previewFragment;
+};
+
+/**
+ * Determines the number of pages to travese on the app based on the total
+ * number of books available in the database.
+ * 
+ * @param {array} array with total number of books.
+ * @param {number} page current page.
+ * @returns {number}
+ */
+export const updateRemaining = (array, page) => {
+    let remaining = array.length - (page * BOOKS_PER_PAGE);
+    return remaining;
+};
+
+/**
+ * Event function to show summary overlay after clicking preview button.
  * This is only placed inside a function to allow re-useability.
  * 
- * @param - void
- * @returns - no return.
+ * @param void
+ * @returns no return.
  */
 export const showPreview = () => {
     const summaryList = document.querySelectorAll('[data-preview]');
 
-[...summaryList].forEach(function(buttons) {
-    let summaryButton = buttons;
-    summaryButton.addEventListener('click', () => {
+    [...summaryList].forEach(function (buttons) {
+        let summaryButton = buttons;
+        summaryButton.addEventListener('click', () => {
 
-    let summaryId = summaryButton.getAttribute('data-preview');
-    let searchBooks = books.find((book) => book.id === summaryId);
-    const { author, image, title, description, published } = searchBooks;
+            let summaryId = summaryButton.getAttribute('data-preview');
+            let searchBooks = books.find((book) => book.id === summaryId);
+            const { author, image, title, description, published } = searchBooks;
 
-    let year = new Date(published).getFullYear();
+            let year = new Date(published).getFullYear();
 
-    html.preview.summaryBlur.src = `${image}`;
-    html.preview.summaryImage.src = `${image}`;
-    html.preview.summaryTitle.innerHTML = `${title}`;
-    html.preview.summarySubTitle.innerHTML = `${authors[author]} (${year})`;
-    html.preview.summaryDescription.innerHTML = `${description}`;
-    
-    html.preview.summaryOverlay.showModal();    
+            html.preview.summaryBlur.src = `${image}`;
+            html.preview.summaryImage.src = `${image}`;
+            html.preview.summaryTitle.innerHTML = `${title}`;
+            html.preview.summarySubTitle.innerHTML = `${authors[author]} (${year})`;
+            html.preview.summaryDescription.innerHTML = `${description}`;
+
+            html.preview.summaryOverlay.showModal();
+        });
     });
-});
     html.preview.summaryClose.addEventListener('click', () => {
-    html.preview.summaryOverlay.close();
-});
+        html.preview.summaryOverlay.close();
+    });
+};
+
+/**
+ * This function searches the books array for a string given in the search form.
+ *  
+ * @param {string} title to search for in books
+ * @param {number} index of current book object in books
+ * @param {array} searchResult array where searchresults will be stored
+ */
+export const searchTitle = (title, index, searchResult) => {
+    let titleMatch;
+    if ((title.trim()) && (books[index].title.toLowerCase().includes(title.toLowerCase()))) {
+        titleMatch = books[index];
+        searchResult.push(titleMatch);
+    };
+};
+
+/**
+ * Searches the books array for book objects given an author id from
+ * the search form.
+ * 
+ * @param {string} author id to search for in books
+ * @param {number} index of current book object in books
+ * @param {array} searchResult array where searchresults will be stored
+ */
+export const searchAuthor = (author, index, searchResult) => {
+    let authorMatch;
+    if (author !== 'any' && books[index].author.includes(author)) {
+        authorMatch = books[index];
+        searchResult.push(authorMatch);         
+    };
+};
+
+/**
+ * Searches the books array for book objects given an id from the search form.
+ * 
+ * @param {string} genre id to search, taken from search form.
+ * @param {number} index of current book object in books.
+ * @param {array} searchResult array where searchresults will be stored.
+ */
+export const searchGenre = (genre, index, searchResult) => {
+    let genreMatch;
+    if (genre !== 'any' && books[index].genres.includes(genre)) {
+        genreMatch = books[index];   
+        searchResult.push(genreMatch);      
+    };
+};
+
+/**
+ * Conducts a strict search for a book object in the books array.
+ * @param {*} filters is an object containing information extracted from the search
+ * form.
+ * @param {*} index of current book object in books.
+ * @param {*} searchResult array where searchresults will be stored.
+ * @returns 
+ */
+export const searchAll = (filters, index, searchResult) => {
+    let allMatch = [];
+
+    let titleState = (filters.title.trim()) && (books[index].title.toLowerCase().includes(filters.title.toLowerCase()));
+    let authorState = filters.author !== 'any' && books[index].author.includes(filters.author);
+    let genreState = filters.genre !== 'any' && books[index].genres.includes(filters.genre);
+
+    if (titleState != true) {
+        return 
+    };
+    if (authorState != true) {
+        return
+    };
+    if(genreState != true ) {
+        return 
+    };
+    
+    allMatch = books[index];
+
+    searchResult.push(allMatch);    
+};
+
+/**
+ * Checks if no input was given to the search form and closes the search
+ * overlay when the submit button is clicked.
+ * 
+ * @param {string} title extracted from search form input.
+ * @param {author} author extracted from search form input.
+ * @param {genre} genre extracted from search form input.
+ */
+export const searchNothing = (title, author, genre) => {
+    if (!title.trim() && (author === 'any') && (genre === 'any')) {
+        html.search.searchOverlay.close();
+        html.search.searchSubmit.reset();
+    };
 };
 
 
--- scripts.js
+++ scripts.js
@@ -17,14 +17,45 @@
  *      screen sizes were shown. Either viqa wireframe or demo.
  */
 
-// Import data.
-import { BOOKS_PER_PAGE, authors, genres, books } from "./data.js"
-import { html, showPreview } from "./functions.js";
-import { createPreviewsFragment, updateRemaining } from "./functions.js"
-import {searchAll, searchAuthor, searchGenre, searchNothing, searchTitle} from './Tests.js'
+// Imports from other files.
 
-const range = [0, 36]
-const matches = books; // Try to convert matches to an indepenedent copy.
+import { 
+    BOOKS_PER_PAGE,
+    authors,
+    genres,
+    books,
+} from "./data.js"
+
+import { 
+    html,
+    showPreview,
+    createPreviewsFragment,
+    updateRemaining,
+    searchAll,
+    searchAuthor,
+    searchGenre,
+    searchNothing,
+    searchTitle,
+} from "./functions.js";
+
+/* -----------------------------------------------PAGE LOAD------------------------------------------*/
+
+/**
+ * The following occurs on page load. Selects and displays the first 36 books from the books array.
+ * Sets page number to 0 on page load.
+ * Checks if books exists as the books array.
+ */
+
+/**
+ * Array of range start and end. This range is the range of books to select from the books array on page load.
+ * @type {Array}
+ */
+const range = [0, 36];
+
+/**
+ * Stores current page number.
+ * @type {number}
+ */
 let page = 1;
 
 /* Checks if books is not empty/undefined, and if it is an array.
@@ -37,17 +68,12 @@ if (!books || !Array.isArray(books)) throw new Error('Source required');
  */
 if (!range && range.length === 2) throw new Error('Range must be an array with two numbers');
 
-const day = {
-    dark: '10, 10, 20',
-    light: '255, 255, 255',
-};
-
-const night = {
-    dark: '255, 255, 255',
-    light: '10, 10, 20',
-};
-
-let fragment = createPreviewsFragment(matches, 0, 36)
+/**
+ * Blank document fragment which will be appended to the 
+ * div with list items denoted by the 'data-list-items' attribute.
+ * @type {HTMLElement}
+ */
+let fragment = createPreviewsFragment(books, 0, 36)
 
 html.view.mainHtml.appendChild(fragment);
 
@@ -57,13 +83,26 @@ window.scrollTo({ top: 0, behavior: 'smooth' }); ///scroll to top on reload.
 
 /* Genres */
 
+/**
+ * Html fragment with genre names inside 'options' tags. Used
+ * to create the search form genre options.
+ * 
+ * @type {HTMLFrangment}
+ */
 const genresHtml = document.createDocumentFragment();
+
+/**
+ * Html 'option' element to form the options for genre & author. This will be
+ * appended to the html and replaced to rom the next option. 
+ * @type {HTMLElement}
+ */
 let element = document.createElement('option');
 element.value = 'any'
 element.innerText = 'All Genres';
 genresHtml.appendChild(element);
 
-for (const [id, name] of Object.entries(genres)) {
+
+for (const [id, name] of Object.entries(genres)) {     
     element = document.createElement('option');
     element.value = id;
     element.innerText = name;
@@ -73,9 +112,14 @@ for (const [id, name] of Object.entries(genres)) {
 html.search.searchGenres.appendChild(genresHtml);
 
 
-/* Authors */
-
+/**
+ * Html fragment with author names inside 'options' tags. Used
+ * to create the search form author options.
+ * 
+ * @type {HTMLFrangment}
+ */
 const authorsHtml = document.createDocumentFragment();
+
 element = document.createElement('option');
 element.value = 'any';
 element.innerText = 'All Authors';
@@ -92,23 +136,54 @@ html.search.searchAuthors.appendChild(authorsHtml);
 
 /* Search Overlay */ 
 
+/**
+ * Listens for the click event on the search overlay close button. Closes the overlay
+ * and resets the search form when clicked.
+ */
  html.search.searchCancel.addEventListener('click', () => {
     html.search.searchOverlay.close();
     html.search.searchSubmit.reset();
  });
 
+
+ /**
+  * Listens for the click event on the search button. Opens the search overlay when clicked.
+  */
 html.search.searchButton.addEventListener('click', () => {
 html.search.searchOverlay.showModal();
 }); 
 
+/**
+ * Creates an empty array to store search results.
+ * Extracts the search form data and saves it in an object.
+ * Determines if a search is strict or not. A strict search is when all search parameters
+ * have been specifies on the search form. i.e. author, genre and title.
+ * Iterates through the books array to find matching book objects and pushes these to the
+ * search storage array.
+ * Creates an html fragment from the search results for the list book buttons and view more
+ * button and appends them to the list html.
+ * If no results are found, displays the list message which is set as hidden on page load.
+ * To show the message, the hidden div is copied and the enyirety of the html list replaced with
+ * the copy.
+ * Closes the overlay, resets the search form and scrolls to top on submit. *  
+ */
 html.search.searchSubmit.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const formData = new FormData(event.target);
     const filters = Object.fromEntries(formData);
-    const result = Object.entries(filters); 
+    const result = Object.entries(filters);
+
+    /**
+     * Stores search results as book objects from the books array.
+     * @type {Array}
+     */
     let searchResult = [];
 
+    /**
+     * Stores search type status.
+     * @type {Boolean}
+     */
     const strictSearch = ((filters.title.trim() !== '') && (filters.author !== 'any') && (filters.genre !== 'any')) ? true : false;
     
     for (let x = 0; x < books.length; x++) {
@@ -127,6 +202,12 @@ html.search.searchSubmit.addEventListener('submit', (event) => {
     };
 
     if (searchResult.length > 0) {
+
+        /**
+         * Fragment of results to be appended to html list. Results appear as buttons on the
+         * html.
+         * @type {HTMLElement}
+         */
         let resultFragment = createPreviewsFragment(searchResult);
                 
         html.view.mainHtml.replaceChildren(resultFragment);
@@ -139,17 +220,22 @@ html.search.searchSubmit.addEventListener('submit', (event) => {
     };
     
     if (searchResult.length === 0 && strictSearch) {
-        
         html.search.seachMessage.setAttribute('class', 'list__message_show');
 
+        /**
+         * Stores html fragment with contents of the div with the search message. Message appears
+         * when no results are found for a search.
+         * @type {HTMLElement}
+         */
         const firstElementChild = html.search.seachMessage;
         
         html.view.mainHtml.innerHTML = '';
         html.view.mainHtml.replaceChildren(firstElementChild);
         html.search.seachMessage.style.display = 'block';
-
-        console.log(html.view.mainHtml)
-
+        
+        /**
+         * Updates the button to view more books and disables it for search results.
+         * */
         html.scroll.moreButton.innerHTML = /* html */ `
         <span>Show more</span>
         <span class="list__remaining"> (0)</span>
@@ -166,10 +252,27 @@ html.search.searchSubmit.addEventListener('submit', (event) => {
 
 /* -------------------------------DISPLAY SETTINGS--------------------------- */ /* COMPLETED */
 
+/**
+ * The following sets display settings using the display settings button & overlay.
+ * On page load, user system settings are checked and applied to webpage.
+ * Creates an event to check if the display settings button is clicked. If clicked,
+ * the display settings overlay is opened.
+ * User settings are stored and applied to the css using the setProperty() method.
+ * If the cancel button is closed, the overlay is closed and the form reset. 
+ */
+
+
 // Check darkmode/lightmode settings of user's system and assign them to the websites settings.
 html.display.settingsTheme.value = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' : 'day';
 
-let v = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches? 'night' : 'day';
+/**
+ * Saves color values for day and night settings.
+ * @type {object} css
+ */
+const css = {
+    day : ['255, 255, 255', '10, 10, 20'],
+    night: ['10, 10, 20','255, 255, 255']
+};
 
 html.display.settingButton.addEventListener('click', () => {
     html.display.settingsOverlay.showModal();
@@ -180,16 +283,18 @@ html.display.settingsCancel.addEventListener('click', () => {
     html.display.settingsSubmit.reset();
 });
 
-const css = {
-    day : ['255, 255, 255', '10, 10, 20'],
-    night: ['10, 10, 20','255, 255, 255']
-};
-
 html.display.settingsSubmit.addEventListener('submit', (event) => {
     event.preventDefault();
-
+    /**
+     * Collects settings form selection.
+     * @type {object}
+     */
     const formData = new FormData(event.target);
-
+    
+    /**
+     * Stores entries from formData.
+     * @type {object}
+     */
     const selected = Object.fromEntries(formData);
 
     if (selected.theme === 'night') {
@@ -206,7 +311,19 @@ html.display.settingsSubmit.addEventListener('submit', (event) => {
 
 /* -----------------------------PAGE SCROLL--------------------------------- */  /* COMPLETED */
 
-let pagesRemaining = matches.length - (page * BOOKS_PER_PAGE);
+/**
+ * The following allows scrolling down the list of books using a button to show more.
+ * On page load, 36 pages are shown. 
+ * Listens for an event on the show more button. If the event is fired, selects the next 36 books from
+ * the books array and appends them as html fragments.
+ * Updates the show more button to show the remaining books that have not been viewed.
+ */
+
+/**
+ * Number of books remaining calculated using the page number.
+ * @type {number}
+ */
+let pagesRemaining = books.length - (page * BOOKS_PER_PAGE);
 
 html.scroll.moreButton.innerHTML = /* html */ `
     <span>Show more</span>
@@ -217,9 +334,9 @@ html.scroll.moreButton.addEventListener('click', () => {
 if (pagesRemaining <= 0) {
     html.scroll.moreButton.disabled;
 }else {
-    html.view.mainHtml.appendChild(createPreviewsFragment(matches, (page * BOOKS_PER_PAGE), (page + 1) * BOOKS_PER_PAGE));
+    html.view.mainHtml.appendChild(createPreviewsFragment(books, (page * BOOKS_PER_PAGE), (page + 1) * BOOKS_PER_PAGE));
     page = page + 1;
-    pagesRemaining = updateRemaining(matches, page);
+    pagesRemaining = updateRemaining(books, page);
 
     html.scroll.moreButton.innerHTML = /* html */ `
     <span>Show more</span>
@@ -231,4 +348,7 @@ if (pagesRemaining <= 0) {
 
 /* -------------------------------------PREVIEW OVERLAY--------------------------------*/ /* COMPLETED */
 
+/**
+ * Opens books summary overlay.
+ */
 showPreview();
